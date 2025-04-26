c65269c658e47b82935c0deb11e29c5e622943da
DianaG9 <dianaa.gomez@gmail.com>
2025-04-26 17:19:07 -0500

    update

--- src/data.jsx
+++ src/data.jsx
@@ -3,6 +3,10 @@ import { v4 as uuidv4 } from "uuid";
 // const beaverCover= "/assets/covers/CanaryForest.jpg";
 const beaver = "/songs/Beaver-Creek.mp3";
 const daylight = "/songs/Daylight.mp3";
+const keepgoing = "/songs/Keep-Going.mp3";
+const nightfall = "/songs/Nightfall.mp3";
+const reflection = "/songs/Reflection.mp3";
+const undercity = "/songs/Under-the-City-Stars.mp3";
 
 function data() {
 return [
@@ -31,7 +35,7 @@ return [
         cover:
             "https://cms.chillhop.com/media/78391/squarelb118536196d42fe9057174e884ac250c93c52ca8.jpg",
         artist: "Swørn",
-        audio: beaver,
+        audio: keepgoing,
         color: ["#CD607D", "#c94043"],
         id: uuidv4(),
         active: false,
@@ -41,7 +45,7 @@ return [
         cover:
             "https://cms.chillhop.com/media/78391/squarelb118536196d42fe9057174e884ac250c93c52ca8.jpg",
         artist: "Aiguille",
-        audio: daylight,
+        audio: nightfall,
         color: ["#EF8EA9", "#ab417f"],
         id: uuidv4(),
         active: false,
@@ -51,7 +55,7 @@ return [
         cover:
             "https://cms.chillhop.com/media/78391/squarelb118536196d42fe9057174e884ac250c93c52ca8.jpg",
         artist: "Swørn",
-        audio: beaver,
+        audio: reflection,
         color: ["#CD607D", "#c94043"],
         id: uuidv4(),
         active: false,
@@ -61,7 +65,7 @@ return [
         cover:
         "https://cms.chillhop.com/media/78391/squarelb118536196d42fe9057174e884ac250c93c52ca8.jpg",
         artist: "Aso, Middle School, Aviino",
-        audio: daylight,
+        audio: undercity,
         color: ["#205950", "#2ab3bf"],
         id: uuidv4(),
         active: false,
